aa74725925ec3238d90e19e682d85b04a2ebb109
ashleywoolman <ashley.woolman@western.edu>
2020-05-13 17:48:20 -0600

    updated TPA and copy pasted fire from bigger dataset but have not run it here.

--- NM_basalarea_tpa.R
+++ NM_basalarea_tpa.R
@@ -1,9 +1,16 @@
 
 # calculating basal area and trees per acre
 
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
 
 ## Upload *resampled, nontreated, pipo & associated sp* file
-NMpipo<- read.csv("NM_Initialquery.csv")
+NMpipo<- read.csv("PIPOplotsResampledNontrt.csv")
+
+#remove first column
+NMpipo1 <- NMpipo[,-1] 
 
 
 # Trees >4.9 dbh, live, only PIPO
@@ -12,156 +19,243 @@ NMpipo<- read.csv("NM_Initialquery.csv")
 # appropriate expansion factors in the same dataframe, so i can include all tree sizes. Also, 
 # starting with live trees to keep it simple this round. 
 
-query1<- NMpipo %>% filter(DIACALC>4.9, STATUSCD==1, SPCD==122)
-length(unique(query1$SUCP))
+
+#
+## TPA live
+#
+
+################################################   
+# Trees >4.9 dbh, LIVE, only PIPO
+#########for tree count
+NMpipo1$STATUSCD<- factor(NMpipo1$STATUSCD)
+summary(NMpipo1)
+NMpipo2<- NMpipo1 %>% filter(DIACALC>4.9, STATUSCD==1, SPCD==122)
+NMpipo3<- NMpipo2 %>% mutate(STATECD = case_when(STATECD==4 ~ 'Arizona', STATECD==8 ~ 'Colorado', STATECD==35 ~ 'NewMexico', 
+                                                    STATECD==49 ~ 'Utah'))
+
+NMpipo3$STATECD<-factor(NMpipo3$STATECD)
+length(unique(NMpipo3$SUCP))
+# 401 NM,  1516 in 4 corners unique SUCP
+
+#cannot have numeric column name
+NMpipo4<- NMpipo3 %>% mutate(cycle_aw = case_when(cycle_aw==2 ~ 'cycletwo', cycle_aw==3 ~ 'cyclethree'))
+NMpipo4$cycle_aw<- factor(NMpipo4$cycle_aw)
+summary(NMpipo4)
+
+NMpipo4$treeexist<- as.logical(NMpipo4$DIACALC)
+NMpipo5<- NMpipo4 %>% group_by(STATECD, SUCP, cycle_aw, SPCD, STATUSCD) %>% summarise(treecount=sum(treeexist))
+length(unique(NMpipo5$SUCP)) #401
+summary(NMpipo5)
+table(NMpipo5$SUCP)
 # 401 unique SUCP
-query1$treeexist<- as.logical(query1$DIACALC)
-query2<- query1 %>% group_by(SUCP, cycle_aw, SPCD, STATUSCD) %>% summarise(treecount=sum(treeexist))
-length(unique(query2$SUCP))
-# 401 unique SUCP
 
-query3<- query6 %>% filter(cycle_aw==3)
-length(unique(query3$SUCP))
-# this says 372 unique SUCP in cycle 3
-query4<- query6 %>% filter(cycle_aw==2)
-length(unique(query4$SUCP))
-# and this says 357 unique SUCP in cycle 2... so i would expect the final dataframe will contain 357
+# pivot_wider
+NMpipo6<- NMpipo5 %>% pivot_wider(names_from=cycle_aw, values_from = treecount)
+table(NMpipo6$SUCP)
+length(unique(NMpipo6$SUCP))# 401 unique SUCP
 
-query5<- unique(query2[query2$cycle_aw==3,]$SUCP)
-query6<- query2 %>% filter(cycle_aw==3|cycle_aw==2 & SUCP%in%query5)
-length(unique(query6$SUCP))
-# but this says 372. so, there are still 15 SUCP (more in cycle 3) that need to be removed in order for them to match..
-table(query6$SUCP)
+#change NA to 0
+NMpipo6$cycletwo[is.na(NMpipo6$cycletwo)]<-0
+NMpipo6$cyclethree[is.na(NMpipo6$cyclethree)]<-0
 
-# from the table, I copy and pasted the SUCPs that were only listed once and made the query below. But
-# theres gotta be an easier way?
-query7<- query6[!(query6$SUCP=="35_1_39_83486" | query6$SUCP=="35_1_31_85707" |query6$SUCP=="35_1_39_84911"| query6$SUCP=="35_1_39_92187"| 
-                    query6$SUCP=="35_1_45_82657"| query6$SUCP=="35_1_55_88500"| query6$SUCP=="35_1_55_91358"| query6$SUCP=="35_1_6_87111"|
-                    query6$SUCP=="35_2_33_92686"| query6$SUCP=="35_2_47_88271"|query6$SUCP=="35_2_47_90798"| query6$SUCP=="35_2_7_81244"| 
-                    query6$SUCP=="35_3_17_80533"| query6$SUCP=="35_3_3_87636"| query6$SUCP=="35_4_35_83290"),]
-length(unique(query7$SUCP))                  
-table(query7$SUCP)
+#back to pivot_longer
+NMpipo7<- NMpipo6 %>% pivot_longer(cols = cycletwo:cyclethree, names_to = "cycle_aw", 
+                                       names_ptypes = list(cycle_aw=factor(levels=c("cycletwo", "cyclethree"))),
+                                       values_to = "treecount")
 
-# this matches! 357*2= 714
+length(unique(NMpipo7$SUCP))
+summary(NMpipo7)
+# 401 unique, 802 total
 
-# create column for plot count
-query7$plotexists<- as.logical(query7$treecount)
-plotcount<- query7 %>% group_by(cycle_aw, SPCD, STATUSCD) %>% summarise(plotcount=sum(plotexists))
-# 357 in both cycle 2 and 3
+#change cycle_aw back to 1/2
+NMpipo8<- NMpipo7 %>% mutate(cycle_aw = case_when(cycle_aw=="cycletwo" ~ '1', cycle_aw=="cyclethree" ~ '2'))
+NMpipo8$cycle_aw<- factor(NMpipo8$cycle_aw)
+summary(NMpipo8)
 
 
+# i went ahead and just changed these to cycle 1 and 2, to match the rest of the data
 
-##################################
-# Create trees per acre (TPA) 
-#################################
+# now add in TPA
 
 # TPA= #trees * expansion factor. FOr 1/24 acre plots, 6.018046 is expansion factor. 
 # I calculated expansion factor with (4*0.04154172) = 6.018046, where 4 is the number
 # of subplots in plot, and 0.04154172 is the area of a 1/24acre subplot. 
 
-query7$TPA<- query7$treecount*6.018046
+NMpipo8$TPA<- NMpipo8$treecount*6.018046
+NMpipo8$STATECD<-factor(NMpipo8$STATECD)
+
+NMpipoLive<- NMpipo8 %>% rename("Cycle" = "cycle_aw", "Plot" = "SUCP", "State" = "STATECD", "Species" = "SPCD",
+                                 "Status"= "STATUSCD")
 
 
-# I can merge the plot count dataframe with totals per cycle to get the average. 
-query8<- query7 %>% group_by(cycle_aw, SPCD, STATUSCD)%>% summarise(TPAtotal=sum(TPA))
-query9<- merge(query8,plotcount,by=c("cycle_aw", "SPCD", "STATUSCD"))
-#divide for average. 
-query9$avgTPA<- (query9$TPAtotal/query9$plotcount)
 
 
 #plot this
-library(ggpubr)
-library(ggplot2)
-query7$cycle_aw<- factor(query7$cycle_aw)
 
-boxplot(TPA~cycle_aw, data=query7)
-ggplot(query7, aes(x=TPA, color=cycle_aw, fill=cycle_aw))+ geom_histogram(binwidth = 20)
-
-
-
-##################################
-# Create basal area
-#################################
-
-# Realized I should have created the dataframe of query 7 earlier on so I wouldnt have to do the below steps again
-# that still has DBH variable
-# Want df similar to query7 with 357 plots
-
-queryforBA<- unique(query1[query1$cycle_aw==3,]$SUCP)
-queryforBA2<- query1 %>% filter(cycle_aw==3|cycle_aw==2 & SUCP%in%queryforBA)
-length(unique(queryforBA2$SUCP))
-# says 372, it is not working again perfectly, so followed same steps above
-
-queryforBA3<- queryforBA2 %>% filter(cycle_aw==3)
-length(unique(queryforBA3$SUCP))
-# cycle 3 has 372
-queryforBA4<- queryforBA2 %>% filter(cycle_aw==2)
-length(unique(queryforBA4$SUCP))
-# cycle 2 has 357, need to remove those same 15 SUCP
-
-queryforBA5<- queryforBA2[!(queryforBA2$SUCP=="35_1_39_83486" | queryforBA2$SUCP=="35_1_31_85707" |queryforBA2$SUCP=="35_1_39_84911"| queryforBA2$SUCP=="35_1_39_92187"| 
-                queryforBA2$SUCP=="35_1_45_82657"| queryforBA2$SUCP=="35_1_55_88500"| queryforBA2$SUCP=="35_1_55_91358"| queryforBA2$SUCP=="35_1_6_87111"|
-                queryforBA2$SUCP=="35_2_33_92686"| queryforBA2$SUCP=="35_2_47_88271"|queryforBA2$SUCP=="35_2_47_90798"| queryforBA2$SUCP=="35_2_7_81244"| 
-                queryforBA2$SUCP=="35_3_17_80533"| queryforBA2$SUCP=="35_3_3_87636"| queryforBA2$SUCP=="35_4_35_83290"),]
-length(unique(queryforBA5$SUCP)) 
-# it worked, 357 plots.
-
-
-### basal area
-queryforBA5$dbhsquared<- queryforBA5$DIACALC^2 
-queryforBA5$BA_Tree_Sqft<- queryforBA5$dbhsquared*0.005454
-queryforBA5$BA_PerAcre_Sqft<- queryforBA5$BA_Tree_Sqft*6.018046
-
-queryforBA6<- queryforBA5 %>% group_by(SUCP, cycle_aw, SPCD, STATUSCD) %>% 
-  summarise(BAplotTotal_PerAcre_Sqft=sum(BA_PerAcre_Sqft))
-length(unique(queryforBA6$SUCP))
-
-queryforBA6$cycle_aw<- factor(queryforBA6$cycle_aw)
-boxplot(BAplotTotal_PerAcre_Sqft~cycle_aw, data=queryforBA6)
-ggplot(queryforBA6, aes(x=BAplotTotal_PerAcre_Sqft, color=cycle_aw, fill=cycle_aw))+ geom_histogram(binwidth = 20)
-
-
-# merge TPA and basal area into 1 dataframe
-merge1<- merge(query7, queryforBA6)
-BAandTPA<- merge1[ -c(5:6) ]
+boxplot(TPA~Cycle, data=NMpipoLive)
+ggplot(NMpipoLive, aes(x=TPA, color=Cycle, fill=Cycle))+ geom_histogram(binwidth = 20)
 
 
 
 
 
-########################
-# some analysis
-########################
+################################################   
+# Trees >4.9 dbh, dead, only PIPO
+#########for tree count
+NMpipoDead<- NMpipo1 %>% filter(DIACALC>4.9, STATUSCD==2, SPCD==122)
+NMpipoDead2<- NMpipoDead %>% mutate(STATECD = case_when(STATECD==4 ~ 'Arizona', STATECD==8 ~ 'Colorado', STATECD==35 ~ 'NewMexico', 
+                                                    STATECD==49 ~ 'Utah'))
 
-# paired T test of NM
-t.test(TPA~cycle_aw, data=query7, paired=TRUE)
-t.test(TPA~cycle_aw, data=query7, var.equal=T)
+NMpipoDead2$STATECD<-factor(NMpipoDead2$STATECD)
+length(unique(NMpipoDead2$SUCP))
+# 237 unique SUCP
+
+#cannot have numeric column name
+NMpipoDead3<- NMpipoDead2 %>% mutate(cycle_aw = case_when(cycle_aw==2 ~ 'cycletwo', cycle_aw==3 ~ 'cyclethree'))
+NMpipoDead3$cycle_aw<- factor(NMpipoDead3$cycle_aw)
+summary(NMpipoDead3)
+
+NMpipoDead3$treeexist<- as.logical(NMpipoDead3$DIACALC)
+NMpipoDead4<- NMpipoDead3 %>% group_by(STATECD, SUCP, cycle_aw, SPCD, STATUSCD) %>% summarise(treecount=sum(treeexist))
+length(unique(NMpipoDead4$SUCP)) #237
+summary(NMpipoDead4)
+table(NMpipoDead4$SUCP)
+
+# pivot_wider
+NMpipoDead5<- NMpipoDead4 %>% pivot_wider(names_from=cycle_aw, values_from = treecount)
+table(NMpipoDead5$SUCP)
+length(unique(NMpipoDead5$SUCP))# 237 unique SUCP
+
+#change NA to 0
+NMpipoDead5$cycletwo[is.na(NMpipoDead5$cycletwo)]<-0
+NMpipoDead5$cyclethree[is.na(NMpipoDead5$cyclethree)]<-0
+
+#back to pivot_longer
+NMpipoDead6<- NMpipoDead5 %>% pivot_longer(cols = cycletwo:cyclethree, names_to = "cycle_aw", 
+                                       names_ptypes = list(cycle_aw=factor(levels=c("cycletwo", "cyclethree"))),
+                                       values_to = "treecount")
+
+length(unique(NMpipoDead6$SUCP))
+summary(NMpipoDead6)
+# 237 unique, 
+
+#change cycle_aw back to 1/2
+NMpipoDead7<- NMpipoDead6 %>% mutate(cycle_aw = case_when(cycle_aw=="cycletwo" ~ '1', cycle_aw=="cyclethree" ~ '2'))
+NMpipoDead7$cycle_aw<- factor(NMpipoDead7$cycle_aw)
+summary(NMpipoDead7)
 
 
-modNM<-lm(TPA~cycle_aw, data=query7)
-anova(modNM)
-summary(modNM)
-#nothing significant? then add in SUCP
-modNM2<-lm(TPA~cycle_aw+SUCP, data=query7)
-anova(modNM2)
-summary(modNM2)
-# identifies plots where a significant change occured. Next include fire in model
+# now add in TPA
+
+# TPA= #trees * expansion factor. FOr 1/24 acre plots, 6.018046 is expansion factor. 
+# I calculated expansion factor with (4*0.04154172) = 6.018046, where 4 is the number
+# of subplots in plot, and 0.04154172 is the area of a 1/24acre subplot. 
+
+NMpipoDead7$TPA<- NMpipoDead7$treecount*6.018046
+NMpipoDead7$STATECD<-factor(NMpipoDead7$STATECD)
+
+NMpipoDead8<- NMpipoDead7 %>% rename("Cycle" = "cycle_aw", "Plot" = "SUCP", "State" = "STATECD", "Species" = "SPCD",
+                                 "Status"= "STATUSCD")
+
+
+#plot this
+
+boxplot(TPA~Cycle, data=NMpipoDead8)
+ggplot(NMpipoDead8, aes(x=TPA, color=Cycle, fill=Cycle))+ geom_histogram(binwidth = 20)
 
 
 
 
 
+###########fire! copy and pasted from the larger dataset, have not gone through with NM specific
+firetry<- NMpipo1 %>% filter(DIACALC>4.9, STATUSCD==1 | STATUSCD==2, SPCD==122)
+length(unique(firetry$SUCP)) #1612 (other is 1622)
+firetry2<- firetry %>% mutate(STATECD = case_when(STATECD==4 ~ 'Arizona', STATECD==8 ~ 'Colorado', STATECD==35 ~ 'NewMexico', 
+                                                  STATECD==49 ~ 'Utah'))
 
-#######################
-# add in fire
-########################
+firetry2$STATECD<-factor(firetry2$STATECD)
+length(unique(firetry2$SUCP))
+# 1612 unique SUCP
 
-#try to merge NM BAandTPA with cond talbe for disturbances.
-# can use "notreatmentcond" from previous script
+#cannot have numeric column name
+firetry3<- firetry2 %>% mutate(cycle_aw = case_when(cycle_aw==2 ~ 'cycletwo', cycle_aw==1 ~ 'cycleone'))
+firetry3$cycle_aw<- factor(firetry3$cycle_aw)
+summary(firetry3)
 
-newfire<- notreatmentcond %>% filter(DSTRBCD1==30| DSTRBCD1==31| DSTRBCD3==32)
-newfire2<- newfire[, c(8:9, 20)]
-BAandTPA_fire<- merge(BAandTPA, newfire2, all=TRUE)
-length(unique(BAandTPA_fire$SUCP))
-# gives me 398 SUCP, and 768 total... need to figure this out
-table(BAandTPA_fire$SUCP)
+firetry3$treeexist<- as.logical(firetry3$DIACALC)
+firetry4<- firetry3 %>% group_by(STATECD, SUCP, cycle_aw, SPCD, STATUSCD) %>% summarise(treecount=sum(treeexist))
+firetry4$STATUSCD<- factor(firetry4$STATUSCD)
+firetry4$SPCD<- factor(firetry4$SPCD)
+length(unique(firetry4$SUCP)) #1612
+summary(firetry4)
+table(firetry4$SUCP)
+
+# pivot_wider
+firetry5<- firetry4 %>% pivot_wider(names_from=cycle_aw, values_from = treecount)
+length(unique(firetry5$SUCP))# 1612 unique SUCP
+
+#change NA to 0
+firetry5$cycletwo[is.na(firetry5$cycletwo)]<-0
+firetry5$cycleone[is.na(firetry5$cycleone)]<-0
+
+#back to pivot_longer
+firetry6<- firetry5 %>% pivot_longer(cols = cycleone:cycletwo, names_to = "cycle_aw", 
+                                     names_ptypes = list(cycle_aw=factor(levels=c("cycleone", "cycletwo"))),
+                                     values_to = "treecount")
+
+length(unique(firetry6$SUCP))
+summary(firetry6)
+
+
+#change cycle_aw back to 1/2
+firetry7<- firetry6 %>% mutate(cycle_aw = case_when(cycle_aw=="cycleone" ~ '1', cycle_aw=="cycletwo" ~ '2'))
+firetry7$cycle_aw<- factor(firetry7$cycle_aw)
+summary(firetry7)
+length(unique(firetry7$SUCP))
+# 1612!
+
+
+
+condfire<- fcorners3 %>% filter(DSTRBCD1==30| DSTRBCD1==31| DSTRBCD3==32 |
+                                  DSTRBCD2==30| DSTRBCD2==31| DSTRBCD2==32 |
+                                  DSTRBCD3==30| DSTRBCD3==31| DSTRBCD3==32)
+condfire2<- condfire %>% mutate(STATECD = case_when(STATECD==4 ~ 'Arizona', STATECD==8 ~ 'Colorado', STATECD==35 ~ 'NewMexico', 
+                                                    STATECD==49 ~ 'Utah'))
+# delete rows
+condfire3<- select(condfire2, -c(1:3, 5:7, 10:19, 26:38))
+
+#merge
+firetry8<- merge(firetry7, condfire3, all.x = TRUE)
+
+# NAs to 0s 
+#change NA to 0
+firetry8$DSTRBCD1[is.na(firetry8$DSTRBCD1)]<-0
+firetry8$DSTRBCD2[is.na(firetry8$DSTRBCD2)]<-0
+firetry8$DSTRBCD3[is.na(firetry8$DSTRBCD3)]<-0
+
+firetry8$DSTRBYR1[is.na(firetry8$DSTRBYR1)]<-0
+firetry8$DSTRBYR2[is.na(firetry8$DSTRBYR2)]<-0
+firetry8$DSTRBYR3[is.na(firetry8$DSTRBYR3)]<-0
+
+length(unique(firetry8$SUCP))
+
+#how many plots with fire in cycle 1?
+firetry9<- firetry8 %>% filter(cycle_aw==1)
+firetry10<- firetry9 %>% filter(DSTRBCD1==30| DSTRBCD1==31| DSTRBCD1==32 |
+                                  DSTRBCD2==30| DSTRBCD2==31| DSTRBCD2==32 |
+                                  DSTRBCD3==30| DSTRBCD3==31| DSTRBCD3==32)
+length(unique(firetry10$SUCP))
+
+firetry30<- firetry9 %>% filter(DSTRBCD1==30| DSTRBCD2==30| DSTRBCD3==30)
+length(unique(firetry30$SUCP))
+#56
+firetry31<- firetry9 %>% filter(DSTRBCD1==31| DSTRBCD2==31| DSTRBCD3==31)
+length(unique(firetry31$SUCP))
+#56
+firetry32<- firetry9 %>% filter(DSTRBCD1==32| DSTRBCD2==32| DSTRBCD3==32)
+length(unique(firetry32$SUCP))
+#1
+
+
+
+
